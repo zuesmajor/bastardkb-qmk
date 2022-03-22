663d6ae549c09b499d27001d82febb57c51530c2
quentin <qlebastard@gmail.com>
2022-03-22 17:24:19 +0100

    enable joystick

--- keyboards/bastardkb/charybdis/3x5/keymaps/bstiq/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/bstiq/rules.mk
@@ -7,13 +7,14 @@ else ifeq ($(MCU),STM32F411)
 	EEPROM_DRIVER = vendor
 endif
 
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-VIALRGB_ENABLE = yes
+# VIA_ENABLE = yes
+# VIAL_ENABLE = yes
+# VIALRGB_ENABLE = yes
 
 THUMBSTICK_ENABLE  	   = yes
 
 ifeq ($(strip $(THUMBSTICK_ENABLE)), yes)
     POINTING_DEVICE_ENABLE = yes
-    POINTING_DEVICE_DRIVER = analog_joystickc
+    POINTING_DEVICE_DRIVER = analog_joystick
+    OPT_DEFS += -DTHUMBSTICK_ENABLE
 endif
