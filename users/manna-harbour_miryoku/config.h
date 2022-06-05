8a3953c3a24b21c9acd721ed613f347a34e0bca1
Charly Delay <0xcharly@codesink.dev>
2022-06-05 19:06:49 +0900

    bastardkb/skeletyl: add Vial-enabled Miryoku keymap

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -18,26 +18,28 @@
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-#define AUTO_SHIFT_NO_SETUP
+// #define AUTO_SHIFT_NO_SETUP
 
 // Mouse key speed and acceleration.
 #undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_DELAY 0
 #undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL       16
+#define MOUSEKEY_INTERVAL 16
 #undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY    0
+#define MOUSEKEY_WHEEL_DELAY 0
 #undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED      6
+#define MOUSEKEY_MAX_SPEED 6
 #undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX    64
+#define MOUSEKEY_TIME_TO_MAX 64
 
 // Thumb Combos
-#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
-  #define COMBO_COUNT 8
-  #define COMBO_TERM 200
-  #define EXTRA_SHORT_COMBOS
+#if defined(MIRYOKU_KLUDGE_THUMBCOMBOS)
+#    define COMBO_COUNT 8
+#    define COMBO_TERM 200
+#    define EXTRA_SHORT_COMBOS
 #endif
 
 #include "custom_config.h"
 
+#define VIAL_KEYBOARD_UID \
+    { 0x73, 0x18, 0xF9, 0xD5, 0x18, 0x11, 0x99, 0xFF }
