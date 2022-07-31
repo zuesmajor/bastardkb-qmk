34a48b588e5daa1b49df79653b09bc462c4f4604
Charly Delay <0xcharly@codesink.dev>
2022-07-31 14:34:12 +0900

    bastardkb/dilemma: fix circular scroll macro

--- keyboards/bastardkb/dilemma/config.h
+++ keyboards/bastardkb/dilemma/config.h
@@ -47,4 +47,4 @@
 // Configure for the Cirque model used on the Dilemma.
 #define CIRQUE_PINNACLE_DIAMETER_MM 35
 #define CIRQUE_PINNACLE_CURVED_OVERLAY
-#define CIRQUE_PINNACLE_CIRCULAR_SCROLL_ENABLE
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE // Circular scroll.
