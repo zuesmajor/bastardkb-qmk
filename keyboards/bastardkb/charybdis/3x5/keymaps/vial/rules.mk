a1dffe855c55048f2876cd4ec3feb0fcea78548a
Charly Delay <0xcharly@codesink.dev>
2022-08-03 18:46:35 +0900

    Add vial keymaps

---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vial/rules.mk
@@ -0,0 +1,10 @@
+# Space saving settings.
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+QMK_SETTINGS = no
+SPACE_CADET_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = no
---
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vial/rules.mk
@@ -0,0 +1,9 @@
+# Space saving settings.
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+QMK_SETTINGS = no
+KEY_OVERRIDE_ENABLE = no
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
