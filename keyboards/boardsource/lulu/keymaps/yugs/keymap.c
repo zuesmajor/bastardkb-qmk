7a55633fb0d0ab6c08bdc044393f4c5a0daca25c
zuesmajor <patrick.email>
2022-12-21 23:08:12 -0800

    changing the layers to be toggable-

--- keyboards/boardsource/lulu/keymaps/yugs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/yugs/keymap.c
@@ -13,6 +13,8 @@ enum layers {
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define SEND_LOWER TG(_LOWER)
+#define SEND_RAISE TG(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LOWER |LGUI | /Space  /       \Enter \  |RAISE |RGUI| RGUI |
+ *                   | LAlt | SLOWER |LGUI | /Space  /       \Enter \  |SRAISE | RAlt | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -36,50 +38,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          KC_LALT,    LOWER, KC_LGUI, KC_SPC,  KC_ENT,   RAISE,   KC_LALT, KC_RGUI
+                          KC_LALT,    SEND_LOWER, KC_LGUI, KC_SPC,  KC_ENT,   SEND_RAISE,   KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   F1   |   F2   |   F3   |   F4   |  F5 |                    |   ^  |   \  |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  UP  |  F4  |  F5  |  F6  |                    |  @   |  #   |  $   |   %  |      |      |
+ * |  F1  |  (   |  UP  |  )   |  F5  |  F6  |                    |  @   |  #   |  $   |   %  |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  | LEFT | DOWN | RIGHT|   $  |   %  |-------.    ,-------|   `  |   (  |   )  |   ~  |      |      |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|   =   |   +  |   <  |   >  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_CIRC, KC_BSLS, _______,_______, _______, _______,
-  KC_F1,   KC_F2,   KC_UP,   KC_F4,   KC_F5,   KC_F6,                     KC_AT,   KC_HASH,   KC_DLR,   KC_PERC,  _______,  _______,
+  KC_F1,   KC_LPRN,   KC_UP,   KC_RPRN,   KC_F5,   KC_F6,                     KC_AT,   KC_HASH,   KC_DLR,   KC_PERC,  _______,  _______,
   KC_CIRC, KC_LEFT, KC_DOWN,   KC_RGHT, KC_DLR,  KC_PERC,                   KC_GRV, KC_LPRN, KC_RPRN, KC_TILD, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_EQL, KC_PLUS, KC_LT, KC_GT, _______, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+                             _______, _______, SEND_LOWER, _______, _______,  _______, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |MOD   | MODR |  S+  |  S-  |  B+  |  B-  |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |  UP  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |   `  |   (  |  UP  |   )  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  LEFT  |  DOWN  |  RIGHT  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
- * |------+------+------+------+------+------|RGB_TOG|    |    ]  |------+------+------+------+------+------|
+ * |  F1  |  LEFT  |  DOWN  |  RIGHT  |  F5  |  F6  |             -------.    ,-------|      | Left | Down |  Up  |Right |      |
+ * |------+------+------+------+------+------|   [  |    |    ]  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LAlt | LLOCK|LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_RAISE] = LAYOUT(
   RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                     _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_UP,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+  KC_GRV,  KC_LPRN,    KC_UP,    KC_RPRN,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   KC_F1,   KC_LEFT,   KC_DOWN,   KC_RGHT ,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   RGB_TOG, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_LBRC, KC_RBRC,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+                             _______, _______, _______,  _______, _______,  SEND_RAISE, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
