e895a9537ce43d3d30d8aeb65730fc3cb4fdfd96
Patrick <patrick_a_ryan@apple.com>
2022-12-22 11:42:12 -0800

    adding 2 shifts to caps lock but it turns off with special characters

--- keyboards/boardsource/lulu/keymaps/yugs/config.h
+++ keyboards/boardsource/lulu/keymaps/yugs/config.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 #define SPLIT_LED_STATE_ENABLE
 #define SPLIT_LAYER_STATE_ENABLE // Share layer state between sides
-#define TAPPING_TOGGLE 2
 #define OLED_BRIGHTNESS 200
+#define CAPS_WORD_IDLE_TIMEOUT 5000
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
