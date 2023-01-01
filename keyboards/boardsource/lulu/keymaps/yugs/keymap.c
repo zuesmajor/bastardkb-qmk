1db1079deaf7e2d9a04aa5cfffbbff7760583ae1
Patrick Ryan <patrickryan@Patricks-MacBook-Air.local>
2022-12-31 16:11:17 -0800

    removing hold lower

--- keyboards/boardsource/lulu/keymaps/yugs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/yugs/keymap.c
@@ -13,7 +13,7 @@ enum layers {
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
-#define SEND_LOWER TG(_LOWER)
+// #define SEND_LOWER TG(_LOWER)
 #define SEND_RAISE TG(_RAISE)
 
 // Tap Dance declarations
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | SLOWER |LGUI | /Space  /       \Enter \  |SRAISE | volD | volU |
+ *                   | LAlt | LOWER |LGUI | /Space  /       \Enter \  |SRAISE | volD | volU |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          KC_LALT,    SEND_LOWER, KC_LGUI, KC_SPC,  KC_ENT,   SEND_RAISE,   KC_RCTL, TD(TD_RALT_CAPS)
+                          KC_LALT,    LOWER, KC_LGUI, KC_SPC,  KC_ENT,   SEND_RAISE,   KC_RCTL, TD(TD_RALT_CAPS)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_LPRN,   KC_UP,   KC_RPRN,   KC_F5,   KC_F6,                     KC_AT,   KC_HASH,   KC_DLR,   KC_PERC,  _______,  _______,
   KC_CIRC, KC_LEFT, KC_DOWN,   KC_RGHT, KC_DLR,  KC_PERC,                   KC_GRV, KC_LPRN, KC_RPRN, KC_TILD, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_EQL, KC_PLUS, KC_LT, KC_GT, _______, _______,
-                             _______, _______, SEND_LOWER, _______, _______,  KC_MUTE, KC_VOLD, KC_VOLU
+                             _______, _______, LOWER, _______, _______,  KC_MUTE, KC_VOLD, KC_VOLU
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
