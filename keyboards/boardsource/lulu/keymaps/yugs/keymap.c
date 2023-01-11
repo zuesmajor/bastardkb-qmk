d5c81b62cc2e32772b354d7ab3b99a69a9276b67
Patrick Ryan <patrickryan@Patricks-MacBook-Air.local>
2023-01-10 21:13:44 -0800

    adding the ff layer selection help of reddit

--- keyboards/boardsource/lulu/keymaps/yugs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/yugs/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LOWER |LGUI | /Space  /       \Enter \  |SRAISE | RCTL | RALT |
+ *                   | LAlt | LOWER |LGUI | /Space  /       \Enter \  |SRAISE | LGUI | RALT |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          KC_LALT,    LOWER, KC_LGUI, KC_SPC,  KC_ENT,   SEND_RAISE,   KC_RCTL, TD(TD_RALT_CAPS)
+                          KC_LALT,    LOWER, KC_LGUI, KC_SPC,  KC_ENT,   SEND_RAISE,   KC_LGUI, TD(TD_RALT_CAPS)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
