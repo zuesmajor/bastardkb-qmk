efa5866fc9c61f4ee8c646ed912c7c8a38f9b58c
zuesmajor <patrick.email>
2023-01-23 10:10:14 -0800

    adding a plus sign tap dance

--- keyboards/boardsource/lulu/keymaps/yugs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/yugs/keymap.c
@@ -19,7 +19,8 @@ enum layers {
 // Tap Dance declarations
 enum {
     TD_RALT_CAPS,
-    TD_RBRC_EQL
+    TD_RBRC_EQL,
+    TD_H_PLUS
 };
 
 // Tap Dance definitions
@@ -27,6 +28,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_RALT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RALT, KC_CAPS),
     [TD_RBRC_EQL] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_EQL),
+    [TD_H_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_PLUS)
 };
 
 
@@ -50,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     TD(TD_H_PLUS),    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  TD(TD_RBRC_EQL),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                           KC_LALT,    LOWER, KC_LGUI, KC_SPC,  KC_ENT,   SEND_RAISE,   KC_LGUI, TD(TD_RALT_CAPS)
 ),
