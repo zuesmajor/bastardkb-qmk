349f607fc390156298c36949d29efa242605fe96
Patrick <patrick_a_ryan@apple.com>
2023-04-27 17:33:15 -0700

    removed the tap dance because of vim

--- keyboards/boardsource/lulu/keymaps/yugs/keymap.c
+++ keyboards/boardsource/lulu/keymaps/yugs/keymap.c
@@ -55,17 +55,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     TD(TD_H_PLUS),    TD(TD_J_LBRC),    TD(TD_K_RBRC),    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_LBRC,    KC_RBRC,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  TD(TD_RBRC_EQL),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                           KC_LALT,    LOWER, KC_LGUI, KC_SPC,  KC_ENT,   RAISE,   KC_LGUI, TD(TD_RALT_CAPS)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   F1   |   F2   |   F3   |   F4   |  F5 |                    |   ^  |   \  |      | PvTrack|NTrack| PauTrack|
+ * |   F1   |   F2   |   F3   |   F4   |  F5 |                    |   ^  |   \  |      | {    |   }  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |      |  UP  |      |  F5  |  F6  |                    |  `   |  ~   |  $   |   %  |      |  |   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  | LEFT | DOWN | RIGHT|   $  |   %  |-------.    ,-------|  LEFT| DOWN | UP   | RIGHT|      |      |
+ * |   `  | LEFT | DOWN | RIGHT|   $  |   %  |-------.    ,-------|  +   | DOWN | UP   | RIGHT|      |      |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|   =   |   +  |   <  |   >  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_CIRC, KC_BSLS, _______, KC_MPRV, KC_MNXT, KC_MPLY,
+  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_CIRC, KC_BSLS, _______, KC_LBRC, KC_RBRC, KC_MPLY,
   KC_F1,   _______,   KC_UP,   _______,   KC_F5,   KC_F6,                     KC_GRV,   KC_TILD,   KC_DLR,   KC_PERC,  _______,  KC_BSLS,
-  KC_CIRC, KC_LEFT, KC_DOWN,   KC_RGHT, KC_DLR,  KC_PERC,                   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
+  KC_CIRC, KC_LEFT, KC_DOWN,   KC_RGHT, KC_DLR,  KC_PERC,                   KC_PLUS, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_EQL, KC_PLUS, KC_LT, KC_GT, _______, _______,
                              _______, _______, LOWER, _______, _______,  KC_MUTE, KC_VOLD, KC_VOLU
 ),
