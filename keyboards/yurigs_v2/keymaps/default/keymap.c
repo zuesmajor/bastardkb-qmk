f1e0ef7d34ead56b849df49f04c8b2948b489f1f
patrick <mail@patrickryan.dev>
2023-10-13 09:27:02 -0700

    encoder code

--- keyboards/yurigs_v2/keymaps/default/keymap.c
+++ keyboards/yurigs_v2/keymaps/default/keymap.c
@@ -25,3 +25,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
     )
 };
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  /* With an if statement we can check which encoder was turned. */
+  if (index == 0) { /* First encoder */
+    /* And with another if statement we can check the direction. */
+    if (clockwise) {
+      /* This is where the actual magic happens: this bit of code taps on the
+         Page Down key. You can do anything QMK allows you to do here.
+         You'll want to replace these lines with the things you want your
+         encoders to do. */
+      tap_code(KC_VOLD);
+    } else {
+      /* And likewise for the other direction, this time Page Down is pressed. */
+      tap_code(KC_VOLU);
+    }
+  /* You can copy the code and change the index for every encoder you have. Most
+     keyboards will only have two, so this piece of code will suffice. */
+  } else if (index == 1) { /* Second encoder */
+    if (clockwise) {
+      tap_code16(C(KC_TAB));
+    } else {
+      tap_code16(S(C(KC_TAB)));
+    }
+  }
+  return false;
+}
