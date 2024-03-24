bed25e113bb05aff200a0d899c33002117002499
zuesmajor <mail@patrickryan.dev>
2024-03-24 10:52:26 -0700

    adding dot to the right side for ease of use

--- keyboards/bastardkb/charybdis/3x5/keymaps/via/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/via/keymap.c
@@ -193,7 +193,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_NUMERAL                                                                  \
     KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, _______________DEAD_HALF_ROW_______________, \
     KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, ______________HOME_ROW_GACS_R______________, \
-     KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, _______________DEAD_HALF_ROW_______________, \
+     KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, KC_DOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  \
                        KC_DOT,    KC_0, KC_MINS, XXXXXXX, _______
 
 /**
